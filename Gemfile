3fbced6a0d7b822603f1487340ee27bc24e208a9
Renan Bona <rcbp3094@gmail.com>
2022-12-25 17:58:15 -0300

    Create Gemfile and calculate taxes method

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org/'
+
+gem 'pry'
+gem 'rspec', '~> 3.4'
+gem 'rubocop', '~> 1.41', '>= 1.41.1', require: false
+gem 'shoulda-matchers', '~> 5.3'
--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 ### basic sales 10% expect food, books, medical products
 ### import sales 5% no exceptions
 
@@ -10,20 +12,42 @@
 ## calculate taxes
 ## generate invoice
 
-data = File.readlines('purchase_input.txt')
-food_items = File.readlines('food_list.txt')
-medical_items = File.readlines('medical_items_list.txt')
+data = File.readlines('purchase_input_3.txt')
 
 data.shift # removing first line
+food_items = File.readlines('food_list.txt')
+medical_items = File.readlines('medical_items_list.txt')
+exempt_items = food_items + medical_items + ['book']
+
+def calculate_taxes(item_name, number_of_items, item_price, exempt_items)
+  item_taxes = item_price * 0.1
+
+  # import validation
+  item_taxes += item_price * 0.05 if item_name.match('imported')
+
+  # exempt validation
+  exempt_items.each do |item|
+    item_taxes -= item_price * 0.1 if item_name.match(item)
+  end
+  # exempt validation
+
+  item_taxes * number_of_items
+end
+
+total_taxes = 0
 
 data.each do |line|
   line = line.split(' ')
-  number_of_items = line.shift
-  item_price = line.pop
+  number_of_items = line.shift.to_i
+  item_price = line.pop.to_f
   line.pop
   item_name = line.join(' ')
 
-  print "o numero de items: #{number_of_items} "
-  puts "Preço por item: #{item_price}"
-  puts item_name
+  item_taxes = calculate_taxes(item_name, number_of_items, item_price, exempt_items)
+  total_taxes += item_taxes
+  total_item_price = (item_price * number_of_items) + item_taxes
+
+  puts "#{number_of_items} #{item_name}: #{total_item_price.round(2)}"
 end
+
+puts "Sales Taxes: #{total_taxes.round(2)}"
